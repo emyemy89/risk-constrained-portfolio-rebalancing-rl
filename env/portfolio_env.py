0c7cf99295c3838a7d2235c2d989010124da248a
Emanuel Paraschiv <emiparaschiv345@gmail.com>
2026-06-28 21:04:01 +0200

    Update portfolio_env.py

--- env/portfolio_env.py
+++ env/portfolio_env.py
@@ -1,26 +1,29 @@
 import numpy as np
 
 class PortfolioEnv:
-    def __init__(self, windows, returns, initial_capital=1.0):
+    def __init__(self, windows, asset_names, initial_balance=1.0, risk_lambda=0.1):
         """
         windows: (T, window_size, num_features)
-        returns: (T, num_assets)
+        asset_names: list of 5 tickers
         """
         self.windows = windows
-        self.returns = returns
-        self.initial_capital = initial_capital
-
-        self.T = windows.shape[0]
-        self.num_assets = returns.shape[1]
-
+        self.asset_names = asset_names
+        self.T = len(windows)
+        self.n_assets = len(asset_names)
+        self.initial_balance = initial_balance
+        self.risk_lambda = risk_lambda
         self.reset()
 
     def reset(self):
         self.t = 0
-        self.wealth = self.initial_capital
-        self.done = False
-
+        self.wealth = self.initial_balance
+        self.portfolio_returns = []
         return self._get_obs()
 
     def _get_obs(self):
         return self.windows[self.t]
+
+    def _softmax(self, x):
+        x = np.array(x)
+        exp_x = np.exp(x - np.max(x))
+        return exp_x / np.sum(exp_x)
