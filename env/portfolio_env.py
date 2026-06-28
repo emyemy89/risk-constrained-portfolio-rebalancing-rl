6953fb2b3bdf0cb2e9ccc4f9a45bd014460cddae
Emanuel Paraschiv <emiparaschiv345@gmail.com>
2026-06-28 21:17:23 +0200

    add step function

--- env/portfolio_env.py
+++ env/portfolio_env.py
@@ -27,3 +27,27 @@ class PortfolioEnv:
         x = np.array(x)
         exp_x = np.exp(x - np.max(x))
         return exp_x / np.sum(exp_x)
+
+    def step(self, action):
+        # we must enforce weights>=0 and sum(weights)=1
+        weights = self._softmax(action)
+        current_window = self.windows[self.t] # today's window
+        asset_returns = current_window[-1, :5]
+        portfolio_return = np.dot(weights, asset_returns) # e.g. 0.5TLT + 0.5SPY
+        self.wealth *= np.exp(portfolio_return) # update wealth
+        self.portfolio_returns.append(portfolio_return) # store for risk
+        # compute reward
+        reward = portfolio_return
+        # risk penalty
+        if len(self.portfolio_returns) > 20:
+            vol = np.std(self.portfolio_returns[-20:])
+            reward -= self.risk_lambda * vol
+        self.t += 1
+        done = self.t >= self.T - 1
+        obs = self._get_obs() if not done else None
+        info = {
+            "wealth": self.wealth,
+            "weights": weights,
+            "portfolio_return": portfolio_return
+        }
+        return obs, reward, done, info
