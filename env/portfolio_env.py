411f719533c159ab8a93983b46cfc0f8eb1b518a
Emanuel Paraschiv <emiparaschiv345@gmail.com>
2026-06-29 18:45:58 +0200

    Update portfolio_env.py

--- env/portfolio_env.py
+++ env/portfolio_env.py
@@ -39,8 +39,9 @@ class PortfolioEnv(gym.Env):
         super().reset(seed=seed)
         self.current_step = 0
         self.portfolio_value = self.initial_cash
-        self.weights = np.ones(self.n_assets) / self.n_assets
+        self.prev_weights = np.ones(self.n_assets) / self.n_assets
         self.portfolio_returns = []
+        self.initial_value = self.portfolio_value
         return self._get_obs(), {}
 
     def _get_obs(self):
@@ -55,12 +56,18 @@ class PortfolioEnv(gym.Env):
         # (St, action) -> (St+1, reward)
         # we must enforce weights>=0 and sum(weights)=1
         weights = self._softmax(action)
+        turnover = np.sum(np.abs(weights - self.prev_weights))
         next_returns = self.returns[self.current_step + 1]
         portfolio_return = np.dot(weights, next_returns) # e.g. 0.5TLT + 0.5SPY
         self.portfolio_value *= np.exp(portfolio_return) # update wealth
         self.portfolio_returns.append(portfolio_return) # store for risk
+
         # compute reward
-        reward = portfolio_return
+        benchmark_return = next_returns[0]
+        excess_return = portfolio_return - benchmark_return
+        reward = excess_return
+        reward -= 0.001 * turnover
+
         # risk penalty
         if len(self.portfolio_returns) >= self.volatility_window:
             recent_returns = self.portfolio_returns[-self.volatility_window:]
@@ -69,10 +76,13 @@ class PortfolioEnv(gym.Env):
         self.current_step += 1
         terminated = self.current_step >= len(self.windows) - 2
         next_obs = self._get_obs() if not terminated else None
+        self.prev_weights = weights
+        episode_return = self.portfolio_value / self.initial_value - 1
         info = {
             "portfolio_value": self.portfolio_value,
             "weights": weights,
-            "portfolio_return": portfolio_return
+            "portfolio_return": portfolio_return,
+            "episode_return": episode_return
         }
         return (
             next_obs,
