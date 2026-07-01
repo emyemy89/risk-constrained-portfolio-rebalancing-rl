4aad77beaa16ae2ad271e43bc8cdbce1a679ae42
Emanuel Paraschiv <emiparaschiv345@gmail.com>
2026-07-01 21:44:27 +0200

    make_envs for each split

--- train/make_env.py
+++ train/make_env.py
@@ -10,3 +10,18 @@ def make_env(windows, returns, **kwargs):
         returns=returns,
         **kwargs,
     )
+
+def make_envs(
+    train_windows,
+    train_returns,
+    val_windows,
+    val_returns,
+    test_windows,
+    test_returns,
+    **kwargs,
+):
+    train_env = make_env(train_windows, train_returns, **kwargs)
+    val_env = make_env(val_windows, val_returns, **kwargs)
+    test_env = make_env(test_windows, test_returns, **kwargs)
+
+    return train_env, val_env, test_env
