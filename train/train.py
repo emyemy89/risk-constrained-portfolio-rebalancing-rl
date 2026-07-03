4c5593c192828e5a6669d77cfc3a7c009c4d000a
Emanuel Paraschiv <emiparaschiv345@gmail.com>
2026-07-03 22:58:04 +0200

    define PPO

--- train/train.py
+++ train/train.py
@@ -3,6 +3,14 @@ from train.make_env import make_envs
 
 # Data Loading
 
-train_env = make_envs()
+train_env, val_env, test_env = make_envs()
 
-model = PPO("MlpPolicy", train_env)
+model = PPO(
+     "MlpPolicy",
+            env=train_env,
+            verbose=1,
+            n_steps=2048, # rollout length
+            batch_size=64, # minibatch size
+            gamma=0.99, # long term reward discount
+            gae_lambda=0.95 # advantage smoothing
+    )
