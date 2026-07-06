48887cad32225ee26a0c03563cb82b6475f51d44
Emanuel Paraschiv <emiparaschiv345@gmail.com>
2026-07-06 18:36:09 +0200

    wrap pipeline in one function

--- train/train.py
+++ train/train.py
@@ -1,13 +1,31 @@
 from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import EvalCallback
 
+from data.pipeline import load_training_data
 from train.make_env import make_envs
 
 # Data Loading
+(
+    train_windows,
+    train_returns,
+    val_windows,
+    val_returns,
+    test_windows,
+    test_returns,
+) = load_training_data()
 
+train_env, val_env, test_env = make_envs(
+    train_windows,
+    train_returns,
+    val_windows,
+    val_returns,
+    test_windows,
+    test_returns,
+)
 train_env, val_env, test_env = make_envs()
 
 # Evaluation callback
+# pauses training -> runs policy on val env -> computes avg performance -> saves best model(if improved)
 eval_callback = EvalCallback(
     val_env,
     best_model_save_path="./models/",
@@ -33,3 +51,4 @@ model.learn(
 )
 
 model.save("final_model")
+
