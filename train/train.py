2f7ad64cf50c12e32b214157b2dcd20584e16557
Emanuel Paraschiv <emiparaschiv345@gmail.com>
2026-07-06 18:20:26 +0200

    add evaluation callback

--- train/train.py
+++ train/train.py
@@ -1,9 +1,21 @@
 from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import EvalCallback
+
 from train.make_env import make_envs
 
 # Data Loading
 
 train_env, val_env, test_env = make_envs()
+
+# Evaluation callback
+eval_callback = EvalCallback(
+    val_env,
+    best_model_save_path="./models/",
+    log_path="./logs/",
+    eval_freq=10_000,
+    deterministic=True,
+    render=False,
+)
 
 model = PPO(
      "MlpPolicy",
@@ -14,3 +26,10 @@ model = PPO(
             gamma=0.99, # long term reward discount
             gae_lambda=0.95 # advantage smoothing
     )
+
+model.learn(
+    total_timesteps=200_000,
+    callback=eval_callback,
+)
+
+model.save("final_model")
