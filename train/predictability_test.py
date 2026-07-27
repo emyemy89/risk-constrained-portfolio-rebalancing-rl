09fc97e83a96b2d0d8146889a3abab0e164d99a7
Emanuel Paraschiv <emiparaschiv345@gmail.com>
2026-07-27 17:35:48 +0200

    update to use validation set

--- train/predictability_test.py
+++ train/predictability_test.py
@@ -13,29 +13,25 @@ from data.pipeline import load_training_data
         test_returns,
     ) = load_training_data()
 
-X = train_windows.reshape(train_windows.shape[0],-1)
-X = X[:-1] # align
-y = train_returns[1:, 0]
+X_train = train_windows.reshape(train_windows.shape[0],-1)
+X_train = X_train[:-1] # align
+y_train = train_returns[1:, 0]
 
-# split chronologically
-split = int(len(X) * 0.8)
 
-X_train = X[:split]
-X_test = X[split:]
-y_train = y[:split]
-y_test = y[split:]
+X_val = val_windows.reshape(val_windows.shape[0],-1)
+y_val = val_returns[1:, 0]
 
 # train rnd forest
 model = RandomForestRegressor(n_estimators=200, random_state=42)
 model.fit(X_train, y_train)
 
 # evaluate
-pred = model.predict(X_test)
-rmse = np.sqrt(mean_squared_error(y_test, pred))
+pred = model.predict(X_val)
+rmse = np.sqrt(mean_squared_error(y_val, pred))
 print("RMSE:", rmse)
 
 # compare against naive prediction
-baseline_pred = np.zeros_like(y_test)
+baseline_pred = np.zeros_like(y_val)
 
-baseline_rmse = np.sqrt(mean_squared_error(y_test, baseline_pred))
+baseline_rmse = np.sqrt(mean_squared_error(y_val, baseline_pred))
 print("Baseline RMSE:", baseline_rmse)
