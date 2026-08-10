9dc030a7f637dc5b8a9a9a800f0f077b5cc51312
Emanuel Paraschiv <emiparaschiv345@gmail.com>
2026-08-10 19:23:20 +0200

    wrap inside fold loop

--- train/run_training.py
+++ train/run_training.py
@@ -43,81 +43,81 @@ def run_training(rl_algorithm="PPO"):
             - "PPO" for Proximal Policy Optimization
             - "SAC" for Soft Actor-Critic
         """
-    # Seeds for reproducibility
-    for seed in seeds:
-        random.seed(seed)
-        np.random.seed(seed)
-        torch.manual_seed(seed)
-        # Data Loading
-        (
-            train_windows,
-            train_returns,
-            val_windows,
-            val_returns,
-            test_windows,
-            test_returns,
-        ) = load_data()
-        inspect_observation(train_windows[0])
+    for fold_idx, (train_end, val_start, val_end) in enumerate(folds):
+        # Seeds for reproducibility
+        for seed in seeds:
+            random.seed(seed)
+            np.random.seed(seed)
+            torch.manual_seed(seed)
+            # Data Loading
+            (
+                train_windows,
+                train_returns,
+                val_windows,
+                val_returns,
+                test_windows,
+                test_returns,
+            ) = load_data()
+            inspect_observation(train_windows[0])
 
 
-        # create the environments
-        train_env, val_env, test_env = make_envs(
-            train_windows,
-            train_returns,
-            val_windows,
-            val_returns,
-            test_windows,
-            test_returns,
-        )
-        train_env.reset(seed=seed)
-        val_env.reset(seed=seed)
-
-        if rl_algorithm == "PPO":
-            model = PPO(
-                policy="MlpPolicy",
-                env=train_env,
-                seed=seed,
-                learning_rate=1e-4,
-                n_steps=2048,  # rollout length
-                batch_size=64,  # mini-batch size
-                n_epochs=10,  # how many times PPO reuses the collected rollout
-                gamma=0.99,  # long-term reward discount (how much agent values future rewards)
-                gae_lambda=0.95,  # advantage smoothing(how are they estimated)
-                clip_range=0.2,  # what makes PPO "proximal" and stable.
-                target_kl=0.02,
-                ent_coef=0.01,  # exploration vs. value learning balance (vf_coef)
-                vf_coef=0.5,
-                tensorboard_log="../logs/tensorboard/",  # logs
-                verbose=0,
+            # create the environments
+            train_env, val_env, test_env = make_envs(
+                train_windows,
+                train_returns,
+                val_windows,
+                val_returns,
+                test_windows,
+                test_returns,
             )
-        else:
-            model = SAC(
-                policy="MlpPolicy",
-                env=train_env,
-                seed=seed,
-                learning_rate=3e-4,
-                buffer_size=100_000,
-                learning_starts=1_000,
-                batch_size=256,
-                tau=0.005,
-                gamma=0.99,
-                ent_coef="auto",
-                tensorboard_log="../logs/tensorboard/", # logs
-                verbose=0,
+            train_env.reset(seed=seed)
+            val_env.reset(seed=seed)
+
+            if rl_algorithm == "PPO":
+                model = PPO(
+                    policy="MlpPolicy",
+                    env=train_env,
+                    seed=seed,
+                    learning_rate=1e-4,
+                    n_steps=2048,  # rollout length
+                    batch_size=64,  # mini-batch size
+                    n_epochs=10,  # how many times PPO reuses the collected rollout
+                    gamma=0.99,  # long-term reward discount (how much agent values future rewards)
+                    gae_lambda=0.95,  # advantage smoothing(how are they estimated)
+                    clip_range=0.2,  # what makes PPO "proximal" and stable.
+                    target_kl=0.02,
+                    ent_coef=0.01,  # exploration vs. value learning balance (vf_coef)
+                    vf_coef=0.5,
+                    tensorboard_log="../logs/tensorboard/",  # logs
+                    verbose=0,
+                )
+            else:
+                model = SAC(
+                    policy="MlpPolicy",
+                    env=train_env,
+                    seed=seed,
+                    learning_rate=3e-4,
+                    buffer_size=100_000,
+                    learning_starts=1_000,
+                    batch_size=256,
+                    tau=0.005,
+                    gamma=0.99,
+                    ent_coef="auto",
+                    tensorboard_log="../logs/tensorboard/", # logs
+                    verbose=0,
+                )
+
+            model.learn(
+                total_timesteps=200_000,
             )
 
-        model.learn(
-            total_timesteps=200_000,
-            callback=eval_callback,
-        )
+            # best_model saved automatically during training based on val performance
+            # final_model is the state after the last update
+            model.save(f"../models/final_model_seed_{seed}")
 
-        # best_model saved automatically during training based on val performance
-        # final_model is the state after the last update
-        model.save(f"../models/final_model_seed_{seed}")
-
-        run_debugging_info(
-            model, test_env, test_returns, seed, PPO if isinstance(model, PPO) else SAC
-        )
+            run_debugging_info(
+                model, test_env, test_returns, seed, PPO if isinstance(model, PPO) else SAC
+            )
 
 if __name__ == "__main__":
     run_training()
