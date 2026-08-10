98b139a676f986b7a20199ad75770a2ffc3eeb70
Emanuel Paraschiv <emiparaschiv345@gmail.com>
2026-08-10 18:56:19 +0200

    remove test dataset from pipeline

--- data/pipeline.py
+++ data/pipeline.py
@@ -11,7 +11,8 @@ from data.data_engineering.processing import validate_data, split_data, scale_fe
 from features.windowing import create_windows
 
 
-def load_data(obs_widow_size = 30, rolling_window = 20):
+def load_data(train_end, val_start, val_end,
+              obs_window_size = 30, rolling_window = 20):
     """
     Prepare training, validation, and test datasets for training.
 
@@ -24,31 +25,33 @@ def load_data(obs_widow_size = 30, rolling_window = 20):
     """
     # (1) Load and validate
     data = validate_data(load_etf_data())
+
     #(2) Create financial features
     # Align assets -> Convert raw to log returns -> Calculate volatility and momentum -> Concatenate
     features, returns = create_features(data,rolling_window)
+
     #(3) Create Splits
-    train_features, val_features, test_features = split_data(features)
-    train_returns, val_returns, test_returns = split_data(returns)
+    train_features, val_features = split_data(features,
+        train_end=train_end, val_start=val_start, val_end=val_end)
+    train_returns, val_returns = split_data(returns,
+        train_end=train_end, val_start=val_start, val_end=val_end)
+
+
     # Normalize using z-score standardization fit on train set only
-    train_scaled, val_scaled, test_scaled = scale_features(
-        train_features, val_features, test_features
-    )
+    train_scaled, val_scaled = scale_features(train_features, val_features)
 
     # Convert time series to window observation
-    train_windows, train_dates = create_windows(train_scaled, obs_widow_size)
-    val_windows, val_dates = create_windows(val_scaled, obs_widow_size)
-    test_windows, test_dates = create_windows(test_scaled, obs_widow_size)
+    train_windows, train_dates = create_windows(train_scaled, obs_window_size)
+    val_windows, val_dates = create_windows(val_scaled, obs_window_size)
 
     # return must match the observation, so we shift
-    offset = obs_widow_size - 1
+    offset = obs_window_size - 1
 
     train_returns = train_returns.iloc[offset:].to_numpy()
     val_returns = val_returns.iloc[offset:].to_numpy()
-    test_returns = test_returns.iloc[offset:].to_numpy()
+
     # %%
     return (
         train_windows, train_returns,
         val_windows, val_returns,
-        test_windows, test_returns,
     )
