661c1e536eb106b91bc6da0244dcdbfece4c0a36
Emanuel Paraschiv <emiparaschiv345@gmail.com>
2026-08-23 19:48:40 +0200

    change asset allocation

--- data/extract/load_data.py
+++ data/extract/load_data.py
@@ -10,7 +10,7 @@ def load_etf_data():
     Download historical price data for assets used in the project
     """
     return yf.download(
-    ["SPY", "QQQ", "TLT", "GLD", "VNQ"], # Cash is loaded in /data/data_engineering/features_def.py
+    ["SPY", "QQQ", "EEM", "GLD", "XLE"], # Cash is loaded in /data/data_engineering/features_def.py
             start="2000-01-01",
             end="2026-01-01",
             auto_adjust=True
